eec0722dbf8f7bf1a76e990b3344d8fbdae0f6e5
Anonymous Applicant <anonymous@example.com>
2018-01-15 15:47:31 -0600

    Refactors search. Adds docstrings.

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -2,13 +2,12 @@
   (:require [clojure.string :as st]
             [clj-http.client :as client]))
 
-(defn submit [request]
-  (html5 (str (ocd-ids (form-params request)))))
-
-(defn form-params [request]
+(defn address [request]
   (get request :form-params))
 
 (defn format-val [prepend getter address]
+  "Formats the parameters from the form.
+  Can add other replacements if form input varies."
   (-> address
       (get getter)
       (st/lower-case)
@@ -24,18 +23,32 @@
   (partial format-val "place:" "city"))
 
 (defn ocd-modifiers [address]
+  "List of all modifiers to the ocd-base.
+  Can add others, like county, legislative districts, etc.  "
   [""
    (get-state address)
    (get-place address)])
 
 (defn ocd-ids [address]
-  (vec (map #(st/join "/" [ocd-base %]) (ocd-modifiers address))))
-
+  "Joins all ocd-ids together in a vector."
+  (vec (map #(st/join "/" [ocd-base %])
+            (ocd-modifiers address))))
 
 (defn get-election-data [ocd-ids]
+  "Gets election data from TurboVote API."
   (let [get-base "https://api.turbovote.org/elections/upcoming?district-divisions="
         arg-str (st/join "," ocd-ids)]
     (client/get (str get-base arg-str))))
 
-; curl 'https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:or,ocd-division/country:us/state:or/place:portland'
+(defn submit [request]
+  (-> request
+      (address)
+      (ocd-ids)
+      str
+
+      ;; This is commented out, so the request doesn't hang
+      ;; (get-election-data)
+
+      ;; TODO Reder a page of Elections
+      ))
 
