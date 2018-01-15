dfd980ff3befabda2db6d16200bf6832bc1a32d6
Anonymous Applicant <anonymous@example.com>
2018-01-15 15:59:24 -0600

    Add skeleton elections view file

---
+++ src/my_exercise/elections.clj
@@ -0,0 +1,11 @@
+(ns my-exercise.elections
+  (:require [hiccup.page :refer [html5]]))
+
+(defn election [e-params]
+  ;; TODO hiccup template for one elections
+  )
+
+(defn elections [elections]
+  (map election elections))
+
+;; TODO Target these templates from the search#submit actions
